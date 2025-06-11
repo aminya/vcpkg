67085a5186748ece04b7c0c1f0a3616a840385bc
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2025-06-11 03:04:47 -0700

    [juce] fix debug breaks on Windows Arm and no inline asm

--- ports/juce/portfile.cmake
+++ ports/juce/portfile.cmake
@@ -17,6 +17,7 @@ vcpkg_from_github(
     vcpkg-compile-definitions.diff
     avoid-macos-15-deprecations.patch
     0005-feat-add-forEach-method-to-ProcessorChain.patch
+    debug-break.patch
 )
 file(REMOVE_RECURSE "${SOURCE_PATH}/modules/juce_audio_devices/native/oboe")
 
